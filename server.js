5126ca9a37a047fd2ebf632ba30a247726420ec1
Jason <jwang11211@gmail.com>
2023-06-28 17:45:22 -0400

    updated for web api use

--- server.js
+++ server.js
@@ -1,56 +1,49 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // Parse JSON request bodies
 
 // Assuming you have an array of image objects
-const images = [
-    { id: 1, url: 'https://example.com/image1.jpg', rating: 0 },
-    { id: 2, url: 'https://example.com/image2.jpg', rating: 0 },
-    // Add more image objects as needed
-  ];
-  
-  app.get('/images', (req, res) => {
-    // Return all images
-    res.json(images);
-  });
-  
-  app.get('/images/:id', (req, res) => {
-    const { id } = req.params;
-  
-    // Find the image by its ID
-    const image = images.find((img) => img.id === parseInt(id));
-  
-    if (!image) {
-      // If the image is not found, return a 404 status code and an error message
-      res.status(404).json({ error: 'Image not found' });
-    } else {
-      // If the image is found, return it
-      res.json(image);
-    }
-  });
+let images = [
+  { id: 1, url: 'https://example.com/image1.jpg', rating: 0 },
+  { id: 2, url: 'https://example.com/image2.jpg', rating: 0 },
+  // Add more image objects as needed
+];
 
-  app.post('/images/:id/rating', (req, res) => {
-    const { id } = req.params;
-    const { rating } = req.body;
-  
-    // Find the image by its ID
-    const image = images.find((img) => img.id === parseInt(id));
-  
-    if (!image) {
-      // If the image is not found, return a 404 status code and an error message
-      res.status(404).json({ error: 'Image not found' });
-    } else {
-      // Update the rating of the image
-      image.rating = rating;
-  
-      // Return the updated image
-      res.json(image);
-    }
-  });
-  
-  
+// Retrieve all images
+app.get('/images', (req, res) => {
+  res.json(images);
+});
 
+// Retrieve a specific image by ID
+app.get('/images/:id', (req, res) => {
+  const { id } = req.params;
+  const image = images.find((img) => img.id === parseInt(id));
 
-  const port = 3000; // Change this if you want to use a different port
+  if (!image) {
+    res.status(404).json({ error: 'Image not found' });
+  } else {
+    res.json(image);
+  }
+});
+
+// Update the rating of an image by ID
+app.post('/images/:id/rating', (req, res) => {
+  const { id } = req.params;
+  const { rating } = req.body;
+
+  const image = images.find((img) => img.id === parseInt(id));
+
+  if (!image) {
+    res.status(404).json({ error: 'Image not found' });
+  } else {
+    image.rating = rating;
+    res.json(image);
+  }
+});
+
+const port = process.env.PORT || 3000; // Use environment variable for port or fallback to 3000
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
